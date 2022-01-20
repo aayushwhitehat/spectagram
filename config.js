f4ff3346f54b1233f341531b725cee3d8ca43547
aayushwhitehat <79359878+aayushwhitehat@users.noreply.github.com>
2022-01-20 09:11:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+ apiKey: "AIzaSyDUbNsOh8EZCojSxKS22-34OhUTE1wq_gc",
+  authDomain: "swift-fabric-338606.firebaseapp.com",
+  databaseURL: "https://swift-fabric-338606-default-rtdb.firebaseio.com",
+  projectId: "swift-fabric-338606",
+  storageBucket: "swift-fabric-338606.appspot.com",
+  messagingSenderId: "1041393190435",
+  appId: "1:1041393190435:web:0463b63c72ec9b8d3dd396"
+};
